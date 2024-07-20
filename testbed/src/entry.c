6d56f118dd88232e12835e1c4ba193a075eb01fc
huangxiang3020 <huangxiang3020@gmail.com>
2024-07-20 21:30:01 +0800

    Surface and physical device.

--- testbed/src/entry.c
+++ testbed/src/entry.c
@@ -15,7 +15,7 @@ b8 create_game(game* out_game) {
     out_game->on_resize = game_on_resize;
 
     // Create the game state.
-    out_game->state = kallocate(sizeof(game_state), MEMORY_TAG_UNKNOWN);
+    out_game->state = kallocate(sizeof(game_state), MEMORY_TAG_GAME);
 
     return TRUE;
 }
