6848ace79999d867dd2f1f6bc5b47fc076ec237a
huangxiang3020 <huangxiang3020@gmail.com>
2024-08-11 08:10:06 +0800

    Update

--- testbed/src/entry.c
+++ testbed/src/entry.c
@@ -6,8 +6,8 @@
 b8 create_game(game* out_game) {
     out_game->app_config.start_pos_x = 100;
     out_game->app_config.start_pos_y = 100;
-    out_game->app_config.start_width = 800;
-    out_game->app_config.start_height = 600;
+    out_game->app_config.start_width = 1280;
+    out_game->app_config.start_height = 720;
     out_game->app_config.name = "Testbed";
     out_game->update = game_update;
     out_game->render = game_render;
